9808192ae744c6ac6ab1409c9bc7c48d153851b3
Vignesh <sgvigneshbe@gmail.com>
2025-01-01 13:01:45 +0530

    Modified driver manager

--- src/main/java/reusables/DriverManager.java
+++ src/main/java/reusables/DriverManager.java
@@ -39,7 +39,7 @@ public class DriverManager {
 			throw new Exception ("Edge browser is not yet implemented");
 		
 		case "remote":
-			String grid_url = "http://hub:4444/wd/hub";
+			String grid_url = "http://192.168.0.106:4444/wd/hub";
 			ChromeOptions chromeOptions = new ChromeOptions();
 			
 			driver = new RemoteWebDriver(new URL(grid_url), chromeOptions);
